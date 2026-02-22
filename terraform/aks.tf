147aeabb6f9384bf1ff65131c9fc901231eda42e
Manohar Mikkili <manohar.mikkili@rakbank.ae>
2026-02-22 18:42:52 +0530

    Add ACR and corresponding roleAssignment

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -80,6 +80,31 @@ resource "azurerm_subnet_network_security_group_association" "private_assoc" {
 }
 
 # -----------------------------
+# Azure Container Registry
+# -----------------------------
+resource "azurerm_container_registry" "acr" {
+  name                = "ecommerceacrmmikkil"   # must be globally unique
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+
+  tags = {
+    environment = "prod"
+  }
+}
+
+# -----------------------------
+# AKS → ACR Pull Permission
+# -----------------------------
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+# -----------------------------
 # AKS Cluster
 # -----------------------------
 resource "azurerm_kubernetes_cluster" "aks" {
